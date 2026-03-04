4e8250cbc29a21fafb9c9f1d85935e281d689105
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-03-04 20:27:23 +0530

    rohan

--- Flask/app.py
+++ Flask/app.py
@@ -1,27 +1,7 @@
-from flask import Flask ,request
+from flask import Flask,render_template
 
-app= Flask(__name__)
+app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'hello user this is my first flask app'
-
-@app.route("/about")
-def about():
-    return 'this is about page'
-
-@app.route("/contact")
-def contact():
-    return 'this is contact ! page'
-
-
-@app.route("/setting")
-def setting():
-    return 'this is setting page'
-
-@app.route("/submit",methods=["GET","POST"])
-def submit():
-    if(request.method=="POST"):
-        return "you are send datat"
-    else:
-        return"you are view"
+    return render_template("basic.html")
--- Flask/login_page.py
+++ Flask/login_page.py
@@ -1,2 +1,41 @@
-from flask import Flask 
+from flask import Flask,request, redirect,url_for,session,Response
+app= Flask(__name__)
+app.secret_key= "supersecret"
+# home pages
+@app.route("/",methods=["GET","POST"])
+def login():
+    if request.method =="POST":
+        username = request.form.get("username")
+        password =request.form.get("password")
 
+        if(username=="admin" and password =="123"):
+            session["user"]= username
+            return redirect(url_for("welcome"))
+        else:
+            return Response("invalid credentials",mimetype="text/plain")
+    
+    
+    return'''
+        <h2>Login foam </h2>
+        <form method="POST">
+        username: <input type="text" name="username"><br>
+        password: <input type="text" name="password"><br>
+        <input type="submit" value="Login">
+        </form>
+'''
+
+# welcome page
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2>Welcome,{session["user"]}!</h2>
+        <a href={url_for('logout')}>Logout</a>
+        '''
+    return redirect(url_for("login")) #re dirext to the login page again
+
+#logout
+@app.route("/logout")
+def logout():
+    session.pop("user", None) # remove the user
+    return redirect(url_for("login"))
